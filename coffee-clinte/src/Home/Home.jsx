5d528d205608a1f020bced1216e3f2a7d018e16a
Ahmad-Maimun <ahmadmaimun.wdd@gmail.com>
2025-01-13 01:01:41 +0600

    Our Popular Product Section Title Done

--- coffee-clinte/src/Home/Home.jsx
+++ coffee-clinte/src/Home/Home.jsx
@@ -1,5 +1,24 @@
+import LocalCafeOutlinedIcon from '@mui/icons-material/LocalCafeOutlined';
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="container mx-auto">
+      <div className="text-center mb-12">
+        <span className="text-xl font-normal font-raleway text-[#1B1A1A] mb-2 mt-28 block">
+          --- Sip & Savor ---
+        </span>
+        <h2 className="text-[#331A15] text-[55px] font-rancho font-normal mb-4">
+          Our Popular Products
+        </h2>
+        <button className="py-2 px-6 bg-[#E3B577] border-2 border-[#331A15] rounded-[5px] text-2xl font-normal font-rancho text-white">
+          Add Coffee
+          <LocalCafeOutlinedIcon
+            sx={{ fontSize: '26px' }}
+            className="ml-2 text-[#331A15]"
+          />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
