12fa3d0938f75ac8451bf3068dfb710efdf2b43f
Ahmad-Maimun <ahmadmaimun.wdd@gmail.com>
2025-01-12 20:38:48 +0600

    Adding React Router Dom and Navbar Design Done

---
+++ coffee-clinte/src/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import { navbarLogo } from '../assets/images';
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="bg-cover bg-center bg-[url(./assets/images/more/15.jpg)] flex justify-center items-center py-4">
+        <img className="w-[75px] h-[90px]" src={navbarLogo} alt="" />
+        <span className="font-rancho text-[60px] font-normal text-white ml-4">
+          Espresso Emporium
+        </span>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ coffee-clinte/src/assets/images.js
@@ -0,0 +1,4 @@
+import navbarImg from '../assets/images/more/15.jpg';
+import navbarLogo from '../assets/images/more/logo1.png';
+
+export { navbarImg, navbarLogo };
