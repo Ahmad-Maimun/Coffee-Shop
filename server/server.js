46778ae24cda76d5bc3734858bdf1d2c89619385
Ahmad-Maimun <ahmadmaimun.wdd@gmail.com>
2025-01-14 08:13:22 +0600

    MongoDB Connected

--- server/server.js
+++ server/server.js
@@ -5,14 +5,12 @@ require('dotenv').config();
 const port = process.env.PORT || 8000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-// coffee-shop
-// paHsqTSRwWOUdElR
-
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  'mongodb+srv://<db_username>:<db_password>@cluster0.mshgq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+console.log(process.env.SECRET_USER);
+
+const uri = `mongodb+srv://${process.env.SECRET_USER}:${process.env.SECRET_KEY}@cluster0.mshgq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
