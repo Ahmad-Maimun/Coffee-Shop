79ccd5093744a0695dc6c4af327901a95b2d5767
Ahmad-Maimun <ahmadmaimun.wdd@gmail.com>
2025-01-15 12:32:53 +0600

    Deleted Coffee From Database

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT || 8000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -40,6 +40,15 @@ async function run() {
       const result = await coffeeCollection.insertOne(coffee);
       res.send(result);
     });
+
+    // delete coffee
+    app.delete('/coffee/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coffeeCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 });
     console.log(
